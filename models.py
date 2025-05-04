834a1a45cd774c992241f4c78c4d83f0487c31dd
Hernan Villarroel gonzalez <hernanvillarroelgonzalez@MacBook-Air-de-Hernan.local>
2025-05-03 21:17:41 -0400

    Corrige relaciones en models.py

--- models.py
+++ models.py
@@ -1,27 +1,6 @@
-from enum import Enum as PyEnum
 from typing import Optional, List
-from sqlmodel import SQLModel, Field, Relationship
 from datetime import date
-
-class EspecialidadEnum(str, PyEnum):
-    ESTRUCTURA             = "Estructura"
-    ELECTRICO              = "Eléctrico"
-    CIVIL                  = "Civil"
-    FIERRO                 = "Fierro"
-    VALVULAS               = "Válvulas"
-    INSTRUMENTACION        = "Instrumentación"
-    MECANICA_EQUIPOS       = "Mecánica Equipos"
-    ELECTRICOS_EQUIPOS     = "Eléctricos Equipos"
-    CANERIAS               = "Cañerías"
-
-class Guia(SQLModel, table=True):
-    id_guid: str = Field(primary_key=True)
-    fecha: date
-    proveedor: Optional[str] = None
-    observacion: Optional[str] = None
-
-    # Relación con el modelo Item
-    items: List["Item"] = Relationship(back_populates="guia")
+from sqlmodel import SQLModel, Field, Relationship
 
 class Item(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -30,12 +9,11 @@ class Item(SQLModel, table=True):
     cantidad: int
     especialidad: Optional[str] = None
     id_guid: str = Field(foreign_key="guia.id_guid")
+    guia: Optional["Guia"] = Relationship(back_populates="items")  # Relación con Guia
 
-    # Relación inversa con el modelo Guia
-    guia: Optional[Guia] = Relationship(back_populates="items")
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True, unique=True)
-    hashed_password: str
-    role: str
+class Guia(SQLModel, table=True):
+    id_guid: str = Field(primary_key=True)
+    fecha: date
+    proveedor: Optional[str] = None
+    observacion: Optional[str] = None
+    items: List[Item] = Relationship(back_populates="guia")  # Relación con Item
